b2a33a4e6ae42c8b143138a97f34048c0d5a6a66
Ludovic Nouvel <ludovic@heyteam.com>
2019-03-11 14:18:52 +0100

    init: nuxt project with i18n working

---
+++ lang/common.js
@@ -0,0 +1,12 @@
+module.exports = {
+	en: {
+		common: {
+			hello: 'Hello !',
+		},
+	},
+	fr: {
+		common: {
+			hello: 'Bonjour toi',
+		},
+	},
+}
---
+++ nuxt.config.js
@@ -0,0 +1,73 @@
+const common = require('./lang/common')
+
+const translations = {
+  en: {
+    ...common.en,
+  },
+  fr: {
+    ...common.fr,
+  }
+}
+
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'Mount',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
+
+  modules: [
+    ['nuxt-i18n', {
+      locales: [
+        {
+          name: 'Français',
+          code: 'fr',
+          iso: 'fr-FR',
+        },
+        {
+          name: 'English',
+          code: 'en',
+          iso: 'en-US',
+        },
+      ],
+      // langDir: 'lang/',
+      defaultLocale: 'fr',
+      vueI18n: {
+        fallbackLocale: 'fr',
+        messages: translations
+      }
+    }]
+  ]
+}
+
